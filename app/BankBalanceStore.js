1fb44ecec23495314baa0d2881ac746bbd769cba
SARATOV\Aleksandr_Selikhov <Aleksandr_Selikhov@epam.com>
2016-09-30 15:50:39 +0300

    Changed BankBalanceStore accordingly to the ReduceStore Util implementation

--- app/BankBalanceStore.js
+++ app/BankBalanceStore.js
@@ -1,38 +1,33 @@
-import {Store} from 'flux/utils';
+import {ReduceStore} from 'flux/utils';
 import AppDispatcher from './AppDispatcher';
 import bankConstants from './constants';
 
 let balance = 0; //stored value
 
-class BankBalanceStore extends Store {
-
-    getState() { //getter for the stored value
-        return balance;
+class BankBalanceStore extends ReduceStore {
+    //getInitialState method must be defined
+    getInitialState() {
+        return 0;
     }
-
-    //onDispatch method must be overriden
-    __onDispatch(action) {
+    //state is treated like immutable property
+    //reduce method must be defined
+    reduce(state, action) {
         //actions consist of type and optional data payload, in our case, it is ammount number
         switch (action.type) {
             case bankConstants.CREATED_ACCOUNT:
                 {
-                    balance = 0;
-                    this.__emitChange(); //each time stored value changes emitChange method should be invoked
+                    return 0;
+                    //emitChange method shouldn't be invoked
                 }
-                break;
             case bankConstants.DEPOSITED_INTO_ACCOUNT:
                 {
-                    balance = balance + action.ammount;
-                    this.__emitChange()
+                    return  state + action.ammount;
                 }
-                break;
+
             case bankConstants.WITHDREW_FROM_ACCOUNT:
                 {
-                    balance = balance - action.ammount;
-                    this.__emitChange()
+                    return state - action.ammount;
                 }
-                break;
-
         }
 
     };
